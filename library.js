70e5297d56b4cc89e3356495dbdd3c425f1bf842
hassan <hchoudhury20@hotmail.com>
2023-03-21 20:44:58 +0000

    create function to add objects to array

--- library.js
+++ library.js
@@ -1,3 +1,8 @@
+const make= document.querySelector("button")
+
+
+
+
 function Book(page, title, author) {
     this.page = page;
     this.title = title;
@@ -15,7 +20,24 @@ function Book(page, title, author) {
   addToLibrary(300,"Yee", "Wee");
   addToLibrary(300,"Yee", "Wee");
 
-  for(i=0;i<library.length;i++){
-
+  const bookList = document.getElementById("book-list");
+function createbook(){
+  for (let i = 0; i < library.length; i++) {
+    const book = library[i];
+    const row = document.createElement("tr");
+    const titleCell = document.createElement("td");
+    const authorCell = document.createElement("td");
+    const pageCell = document.createElement("td");
+  
+    titleCell.textContent = book.title;
+    authorCell.textContent = book.author;
+    pageCell.textContent = book.page;
+  
+    row.appendChild(titleCell);
+    row.appendChild(authorCell);
+    row.appendChild(pageCell);
+  
+    bookList.appendChild(row);
   }
-  console.log(library);
+}
+createbook()
